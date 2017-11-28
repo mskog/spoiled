c0a7ca4e522f961841dccef2b6b72050afa9ee23
Magnus Skog <magnus.m.skog@gmail.com>
2017-11-28 13:13:22 +0100

    Using google searches to find the RT url

--- app.js
+++ app.js
@@ -2,59 +2,39 @@ const express = require('express')
 const app = express()
 const puppeteer = require('puppeteer')
 const snakeCase = require('lodash.snakecase')
+const axios = require('axios')
+require('dotenv').config()
 
 app.get('/', function (req, res) {
   (async() => {
     const title = snakeCase(req.query.title)
-    const year = req.query.year
+    const year = req.query.year || ''
 
     const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']})
     const page = await browser.newPage()
 
     // page.on('console', (...args) => console.log('PAGE LOG:', ...args))
 
-    let result = {}
+    const movieUrl = await findMovieUrl(title, year)
+    await page.goto(movieUrl, {waitUntil: 'domcontentloaded'})
 
-    result = await navWithYear(title, year, page, result)
+    let result = Object.assign({title: title, year: year, url: movieUrl}, await extractRtData(page))
 
-    if ('score' in result) {
-      result['title'] = title
-
-      res.json(result)
-      browser.close()
-    } else {
-      result = await navWithoutYear(title, page, result)
-      result['title'] = title
-
-      res.json(result)
-      browser.close()
-    }
+    res.json(result)
+    browser.close()
   })()
 })
 
-async function navWithYear (title, year, page, result) {
-  const urlWithYear = `https://www.rottentomatoes.com/m/${title}_${year}`
-  const navResult = await page.goto(urlWithYear, {waitUntil: 'domcontentloaded'})
-  if (navResult.ok === true) {
-    result['url'] = urlWithYear
-    result['score'] = await extractScore(page)
-  }
-  return result
+async function findMovieUrl (title, year) {
+  const googleKey = process.env.GOOGLE_KEY
+  const googleCx = process.env.GOOGLE_CX
+  const response = await axios.get(`https://www.googleapis.com/customsearch/v1?key=${googleKey}&cx=${googleCx}&q=${title} ${year}`)
+  return response.data['items'][0]['link']
 }
 
-async function navWithoutYear (title, page, result) {
-  const url = `https://www.rottentomatoes.com/m/${title}`
-  const navResult = await page.goto(url, {waitUntil: 'domcontentloaded'})
-  if (navResult.ok === true) {
-    result['url'] = url
-    result['score'] = await extractScore(page)
-  }
-  return result
-}
-
-async function extractScore (page) {
+async function extractRtData (page) {
   return page.evaluate(() => {
-    return document.querySelector('span.meter-value > span').textContent
+    return {score: document.querySelector('span.meter-value > span').textContent}
   })
 }
 
